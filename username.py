92538f898663d0788812acef0544b5ecd40bec6c
jayparmar271 <jayparmar271@gmail.com>
2025-04-28 19:02:49 +0530

    Add username

---
+++ username.py
@@ -0,0 +1,10 @@
+first_name=input("first name?\n")
+last_name=input("last name?\n")
+
+print(first_name +" "+ last_name)
+
+name = first_name
+first_name = last_name
+last_name = name
+
+print(first_name +" "+ last_name)

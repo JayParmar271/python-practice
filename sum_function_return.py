be61020c38950b365e3fd41f07e5086c482c0a43
jayparmar271 <jayparmar271@gmail.com>
2025-04-29 18:42:34 +0530

    Add sum_function_return

---
+++ sum_function_return.py
@@ -0,0 +1,8 @@
+def sum(number1, number2):
+    sum = number1 + number2
+    return sum
+
+number1=int(input("give me a number\n"))
+number2=int(input("give me a number\n"))
+
+print(sum(number1,number2))

9fd570f1556b7f69e7318d2af3b3b0334fef91a3
jayparmar271 <jayparmar271@gmail.com>
2025-04-29 18:35:48 +0530

    Add use function

--- sum_function.py
+++ sum_function.py
@@ -1,6 +1,8 @@
-sum=0
+def sum(number1, number2):
+    sum = number1 + number2
+    print(sum)
+
 number1=int(input("give me a number\n"))
 number2=int(input("give me a number\n"))
-sum = number1+number2
-print(sum)
 
+sum(number1,number2)

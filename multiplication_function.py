0b6dd335b974f8ba2b600a51cc4f769d89938df3
jayparmar271 <jayparmar271@gmail.com>
2025-04-30 18:12:29 +0530

    Add multiplication of number

---
+++ multiplication_function.py
@@ -0,0 +1,8 @@
+def multiplication(number1, number2):
+     multiplication= number1 * number2
+     print( multiplication)
+
+number1=int(input("give me a number\n"))
+number2=int(input("give me a number\n"))
+
+multiplication(number1, number2)

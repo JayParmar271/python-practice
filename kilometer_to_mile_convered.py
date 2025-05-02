2dbfbfa35be6643b34f7a31f662d44e326225fca
jayparmar271 <jayparmar271@gmail.com>
2025-05-02 18:01:38 +0530

    kilometer-to-meter

---
+++ kilometer_to_mile_convered.py
@@ -0,0 +1,6 @@
+def KilometerToMilesConverter(kilometer):
+        miles = kilometer * 0.621
+        print(f"your miles is {miles}")
+
+kilometer=int(input("Enter your kilometer\n"))
+KilometerToMilesConverter(kilometer)

c172b04ad6a1ca2a389f403ef36b2aa1bc1aa7ac
jayparmar271 <jayparmar271@gmail.com>
2025-05-26 18:26:30 +0530

    multiplication number

---
+++ multiplication_return_function.py
@@ -0,0 +1,9 @@
+def multiplication(number1, number2):
+    multiplication= number1 * number2
+    print(multiplication)
+
+number3=int(input("give me a number\n"))
+number4=int(input("give me a number\n"))
+multiplication(number3,number4)
+
+print

4819ed4ce144306964f73a71d35456f13ffc54ae
jayparmar271 <jayparmar271@gmail.com>
2025-05-27 10:07:25 +0530

    Added example

---
+++ multiplication_default_value_pass_arguments_by_name.py
@@ -0,0 +1,7 @@
+def multiplication(number3, number4 = 20):
+    multiplication = number3 * number4
+    return multiplication
+
+number2 = int(input("give me a number\n"))
+number1 = int(input("give me a number\n"))
+print(multiplication(number3 = number1, number4 = number2))

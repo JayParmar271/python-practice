36affd43eba4e7cd0e816574bb5a92562d3aa26c
jayparmar271 <jayparmar271@gmail.com>
2025-05-27 10:10:00 +0530

    Remove

--- multiplication_return_function.py
+++
@@ -1,9 +0,0 @@
-def multiplication(number1, number2):
-    multiplication= number1 * number2
-    print(multiplication)
-
-number3=int(input("give me a number\n"))
-number4=int(input("give me a number\n"))
-multiplication(number3,number4)
-
-print

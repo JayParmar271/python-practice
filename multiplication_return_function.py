b148052d02cd073789ca838cf93042a22514c412
jayparmar271 <jayparmar271@gmail.com>
2025-05-27 09:43:29 +0530

    Fix: Typo

--- multiplication_return_function.py
+++ multiplication_return_function.py
@@ -1,9 +1,7 @@
 def multiplication(number1, number2):
     multiplication= number1 * number2
-    print(multiplication)
+    return multiplication
 
-number3=int(input("give me a number\n"))
-number4=int(input("give me a number\n"))
-multiplication(number3,number4)
-
-print
+number1=int(input("give me a number\n"))
+number2=int(input("give me a number\n"))
+print(multiplication(number1,number2))
